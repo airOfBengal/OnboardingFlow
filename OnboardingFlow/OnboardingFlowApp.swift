b3245c5edaf2100cae78556f8dac8b3b59721750
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-06-13 11:05:55 +0600

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Md. Atiqul Islam on 13/6/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
